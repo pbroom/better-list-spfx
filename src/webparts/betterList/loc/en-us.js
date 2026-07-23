c2c0116a087313e59a96167cc7424fdf4a0ec219
pbroom <116581966+pbroom@users.noreply.github.com>
2026-07-23 16:07:11 -0400

    copy: attribute Better List to M/EX

--- src/webparts/betterList/loc/en-us.js
+++ src/webparts/betterList/loc/en-us.js
@@ -1,6 +1,6 @@
 define([], function() {
   return {
-    PropertyPaneDescription: 'Map a SharePoint list to a searchable, grouped Better List experience.',
+    PropertyPaneDescription: 'Map a SharePoint list to a searchable, grouped Better List experience. Developed by M/EX - Executive Office.',
     PropertyPaneGroupName: 'Better List',
     LoadingMessage: 'Loading list items…',
     ErrorMessage: 'Better List could not load its SharePoint data.'
